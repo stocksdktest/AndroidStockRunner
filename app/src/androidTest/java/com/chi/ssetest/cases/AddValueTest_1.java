f7c04ecd5d5fd2f6737ae3999358fc7c35d8522a
883111LB <2364617489@qq.com>
2019-09-21 00:30:16 +0800

    全部用例修改

--- app/src/androidTest/java/com/chi/ssetest/cases/AddValueTest_1.java
+++ app/src/androidTest/java/com/chi/ssetest/cases/AddValueTest_1.java
@@ -53,7 +53,6 @@ public class AddValueTest_1 {
             throw new Exception(String.format("Testcase(%s) setup failed, config is empty", testcaseName));
         }
     }
-
     @Rule
     public TestcaseConfigRule rule = new TestcaseConfigRule(testcaseConfig);
 
